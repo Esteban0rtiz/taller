40e08dfb6f04dd2200351542dd7772ea735f5888
Esteban0rtiz <esteban.ortizpinsag@gmail.com>
2023-12-14 07:18:27 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [cart, setCart] = useState([]);
+
+  const addToCart = () => {
+    setCart([...cart, item]);
+    setItem('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Mi Carrito de Compras</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese un artículo"
+        value={item}
+        onChangeText={(text) => setItem(text)}
+      />
+      <Button title="Agregar al carrito" onPress={addToCart} />
+      <View>
+        <Text>Artículos en el carrito:</Text>
+        {cart.map((item, index) => (
+          <Text key={index}>{item}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -16,5 +36,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
-});
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    width: '100%',
+  },
+}); 
